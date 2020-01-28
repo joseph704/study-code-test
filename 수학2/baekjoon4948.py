10ad85adf21a6b2d04e0a1bdeb7acaba2bc90699
joseph704 <freelove54@naver.com>
2020-01-28 15:31:09 +0900

    Add baekjoon4948.py which is 에라토스테네스의 채

---
+++ 수학2/baekjoon4948.py
@@ -0,0 +1,13 @@
+def prime_list(n):
+    sieve = [True] * n
+    m = int(n ** 0.5)
+    for i in range(2, m + 1):
+        if sieve[i] == True:
+            for j in range(i+i, n, i):
+                sieve[j] = False
+    return [i for i in range(2, n) if sieve[i] == True]
+while 1:
+    n=int(input())
+    if n==0:break
+    li=prime_list(2*n+1)
+    print(len([i for i in li if i>n]))
