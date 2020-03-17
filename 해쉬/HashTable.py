c09c6ea3e2e77326f0c3a0eab95734de33303339
joseph704 <freelove54@naver.com>
2020-03-17 23:55:35 +0900

    Update HashTable.py

--- 해쉬/HashTable.py
+++ 해쉬/HashTable.py
@@ -7,9 +7,25 @@ def hash_function(key):
     return key % 8
 
 def save_data(data,value):
-    key_address = hash_function(get_key(data))
-    hash_table[key_address] = value
-
+    # chain 기법
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(len(hash_table[hash_address])):
+            if hash_table[hash_address][index][0] == index_key:
+                hash_table[hash_address][index][1] = value
+                return
+        hash_table[hash_address].append([index_key,value])
+    else:
+        hash_table[hash_address] = [[index_key, value]]
 def read_data(data):
-    key_address = hash_function(get_key(data))
-    return hash_table[key_address]
+    # chain 기법
+    index_key = get_key(data)
+    hash_address = hash_function(index_key)
+    if hash_table[hash_address] != 0:
+        for index in range(len(hash_table[hash_address])):
+            if hash_table[hash_address][index][0] == index_key:
+                return (hash_table[hash_address][index][1])
+        return None
+    else:
+        return None
